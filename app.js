61e9a721e1a469ffe5f0d0a86c4911c4453d38bb
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-14 23:35:52 +0530

    created one middelware with give same response for every request

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const app = express();
 
+  let port = 3000;
+ 
+   app.use((req,res)=>{
+    console.log("Hi, I am a Middleware");
+    res.send("Middleware request is sended");
+   });
+
+
+   app.use("/Listing",(req,res)=>{
+    console.log("Listing are showing");
+   })
+
+
+  app.listen(port,()=>{
+    console.log(`port is Runing on ${port}`) 
+  });
