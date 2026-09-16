90db98fc7956da5c00d1fc35753be57b2716bcc3
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-16 23:39:01 +0530

    make it as it is

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const app = express();
 
   // Making Logger Functionality my using middleware 
 
+    app.use((req,res,next)=>{
+      req.time = new Date(Date.now()).toString();
+      console.log(req.method,req.hostname,req.path,req.time);
+      next();
+  });
+
       app.get("/",(req,res)=>{
     res.send("HI, I am Root ");
    })
