fa8a95e58d58b42bbf50443e12f933a4b0918370
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-16 23:22:33 +0530

    make 2 very simple middleware

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const app = express();
 
   let port = 3000;
  
-   app.use((req,res)=>{
-    console.log("Hi, I am a Middleware");
-    res.send("Middleware request is sended");
+   app.use((req,res,next)=>{
+    console.log("Hi, I am 1st Middleware");
+    next();
    });
 
+   app.use((req,res,next)=>{
+    console.log("Hi, I am 2nd Middleware");
+    next();
+   });
 
-   app.use("/Listing",(req,res)=>{
-    console.log("Listing are showing");
+      app.get("/",(req,res)=>{
+    res.send("HI, I am Root ");
    })
+
+   app.get("/listings",(req, res)=>{
+    res.send("Listing are showing");
+   });
 
 
   app.listen(port,()=>{
