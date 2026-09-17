6fb2d2c8b3238b181ba29d2235d6dc2d71fee5a7
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-17 20:45:20 +0530

    Created middleware functions with the help of that function we can use one middleware for multiple paths or requests

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ const app = express();
       });
 
     //   Api access token validation middleware 
-      app.use("/api",(req,res,next)=>{
+       const checkToken =(req,res,next)=>{
         let { token } = req.query;
          if (token === "giveaccess"){
            next();
          }
           res.send("Access Denied!")
-      });
+      };
 
-     app.get("/api",(req,res)=>{
+     app.get("/api",checkToken, (req,res)=>{
          res.send("data ");
        });
 
